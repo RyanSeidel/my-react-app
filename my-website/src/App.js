bf25844905a14ea29ee35b548efa3739aa2a59d3
NuttyRyan <76074138+RyanSeidel@users.noreply.github.com>
2025-01-20 16:32:43 -0600

    pls work!

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Wow im hacking
+          Edit <code>src/App.js</code> and save to reload. Website is Working I must push!
         </p>
         <a
           className="App-link"
